c101956a3d8bfc03ac8583e675b725fbfec21daf
Piotr Przekaza <piotr.przekaza@hotmail.com>
2020-09-12 14:05:50 +0200

    craete empty database by migration

--- CureMed.Database/CureMedAppDbContext.cs
+++ CureMed.Database/CureMedAppDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CureMed.Database.Entites;
+using Microsoft.EntityFrameworkCore;
 
 namespace CureMed.Database
 {
     public class CureMedAppDbContext : DbContext
     {
+        //public DbSet<Doctor> Doctors { get; set; }
         public CureMedAppDbContext(DbContextOptions options) : base(options)
         {
 
