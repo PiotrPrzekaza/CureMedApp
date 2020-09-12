84aa76c0ded5bae1d86c476e20d41ad9f6db3fd5
Piotr Przekaza <piotr.przekaza@hotmail.com>
2020-09-12 13:45:44 +0200

    fillup doctor data table

--- CureMed.Database/Entites/Doctor.cs
+++ CureMed.Database/Entites/Doctor.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace CureMed.Database.Entites
 {
-    class Doctor
+    public class Doctor
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+        public string PhoneNumber { get; set; }
+        public int WorkYears { get; set; }
+        public bool IsAbleToMakePrescriptions { get; set; }
     }
 }
