1ce47d11c7fef847c8af0c24e2fc999dda4bbb5e
Piotr Przekaza <piotr.przekaza@hotmail.com>
2020-09-16 16:51:20 +0200

    copy property from dtos

--- CureMed/Models/DoctorViewModel.cs
+++ CureMed/Models/DoctorViewModel.cs
@@ -4,7 +4,13 @@ namespace CureMed.Models
 {
     public class DoctorViewModel
     {
-        public string Name { get; set; }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+        public int WorkYears { get; set; }
+        public bool IsAbleToMakePrescriptions { get; set; }
         public List<PrescriptionViewModel> Prescriptions { get; set; }
     }
 }
--- CureMed/Models/PrescriptionViewModel.cs
+++ CureMed/Models/PrescriptionViewModel.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace CureMed.Models
 {
     public class PrescriptionViewModel
     {
+        public int Id { get; set; }
         public string Name { get; set; }
+        public DateTime CraetedDate { get; set; }
+        public DoctorViewModel Doctor { get; set; }
         public List<MedicineViewModel> Medicines { get; set; }
     }
 }
